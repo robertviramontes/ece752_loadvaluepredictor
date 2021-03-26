0235a277a17790d80efae1b2a159fac507630b66
Robert Viramontes <rviramontes24@gmail.com>
2021-03-26 17:45:07 -0500

    Started updating the python config.

--- gem5/src/cpu/lvp/LoadValuePredictor.py
+++ gem5/src/cpu/lvp/LoadValuePredictor.py
@@ -18,5 +18,5 @@ class LoadClassificationTable(SimObject):
 
 # class ConstantVerificationUnit(SimObject):
  
-# class LoadValePredictionUnit(SimObject):
+# class LoadValuePredictionUnit(SimObject):
     load_classification_table = Param.LoadClassificationTable("A load classification table")
