1c2edcc205630f87803de960c7ce0d424b7ba628
Vanshika Baoni <vbaoni@garfield.cs.wisc.edu>
2021-04-16 18:16:38 -0500

    Prajyot

--- gem5/src/cpu/lvp/load_value_prediction_table.hh
+++ gem5/src/cpu/lvp/load_value_prediction_table.hh
@@ -1,64 +1,136 @@
-/*
- * Robert Viramontes
- * Created March 30, 2021
- * Implements the load value prediction table of the load value prediction unit. 
- * 
- * 
- */
+/****************************************************************************/
+// Author 	: Prajyot Gupta
+// Department   : Grad Student @ Dept. of Electrical & Computer Engineering
+// Contact      : pgupta54@wisc.edu
+// Project      : ECE 752
+/****************************************************************************/
 
-#ifndef __CPU_LVP_LOADVALUEPREDICTIONTABLE_HH__
-#define __CPU_LVP_LOADVALUEPREDICTIONTABLE_HH__
+#ifndef __CPU_PRED_LVPT_HH__
+#define __CPU_PRED_LVPT_HH__
 
+#include "arch/types.hh"
+#include "base/logging.hh"
 #include "base/types.hh"
-#include "cpu/static_inst.hh"
-#include "sim/sim_object.hh"
+#include "config/the_isa.hh"
 
-#include "enums.hh"
-#include "params/LoadValuePredictionTable.hh"
-
-/**
- * Implements storage of values for the load value locality prediction.
+/** Creating a default Load Value Prediction Table entry
+ *  which will have below attributes
+ *  tag   : Specifies the Opcode of the Load instruction
+ *  taget : Specifies the Load value associated with the tag
+ *  valid : Specifies if the value loaded is valid
  */
-class LoadValuePredictionTable : public SimObject
+
+class LoadValuePredictionTable : public simObject
 {
-  public:
-    /**
-     * Build load classfication table.
-     */
-    LoadValuePredictionTable(const LoadValuePredictionTableParams *params);
-
-    /**
-     * Looks up the given instruction address in the LVPT and returns
-     * a value for the instruction address.
-     * @param inst_addr The address of the instruction to look up.
-     * @param bp_history Pointer to any bp history state.
-     * @return Whether or not the branch is taken.
-     */
-    uint64_t lookup(ThreadID tid, Addr inst_addr);
-
-    /**
-     * Updates the branch predictor with the actual result of a branch.
-     * @param inst_addr The address of the instruction to update.
-     * @param taken Whether or not the branch was taken.
-     */
-    void update(ThreadID tid, Addr inst_addr, bool prediction_correct,
-                bool squashed, const StaticInstPtr & inst, Addr corrTarget);
-
   private:
-    /** Calculates the local index based on the PC. */
-    inline unsigned getLocalIndex(Addr &PC);
+    struct LVPTEntry
+    {
+        LVPTEntry()
+            : tag(0), target(0), valid(false)
+        {}
 
-    /** Size of the local predictor. */
+        /** The entry's tag. */
+        Addr tag;
+
+        /** The entry's target. 
+	 * prajyot :: TODO 
+	 * Change to typedef from TheISA to 
+	 * data: int/signed/unisgned
+	 */
+        //prajyotg :: TheISA::PCState target;
+        unsigned target;
+
+        /** The entry's thread id. */
+        ThreadID tid;
+
+        /** Whether or not the entry is valid. */
+        bool valid;
+    };
+
+  public:
+    /** Creates a LVPT with the given number of entries, number of bits per
+     *  tag, and instruction offset amount.
+     *  prajyotg :: TODO :: look about instr-offset
+     *  @param numEntries Number of entries for the LVPT.
+     *  @param tagBits Number of bits for each tag in the LVPT.
+     *  @param instShiftAmt Offset amount for instructions to ignore alignment.
+     */
+    LoadValuePredictionTable(unsigned numEntries, unsigned tagBits,
+               unsigned instShiftAmt, unsigned numThreads);
+// TODO :: Discuss with Robert
+    LoadValuePredictionTable(const LoadValuePredictionTableParams *params);
+    void reset();
+
+    /** Looks up an address in the LVPT. Must call valid() first on the address.
+     *  @param inst_PC The address of the branch to look up.
+     *  @param tid The thread id.
+     *  @return Returns the predicated load value.
+     *  prajyotg :: TODO :: Update TheISA with data type
+     */
+    //prajyotg :: TheISA::PCState lookup(Addr instPC, ThreadID tid);
+    unsigned lookup(Addr instPC, ThreadID tid);
+
+    /** Checks if the load entry is in the LVPT.
+     *  @param inst_PC The address of the branch to look up.
+     *  @param tid The thread id.
+     *  @return Whether or not the branch exists in the LVPT.
+     */
+    bool valid(Addr instPC, ThreadID tid);
+
+    /** Updates the LVPT with the latest predicted Load Value.
+     *  @param inst_PC The address of the branch being updated.
+     *  @param target_PC The predicted target data.
+     * prajyotg :: TODO
+     *  @param tid The thread id.
+     */
+    //prajyotg void update(Addr instPC, const TheISA::PCState &targetPC,
+    void update(Addr instPC, const unsigned &target,
+                ThreadID tid);
+
+// ROBert: void update(ThreadID tid, Addr inst_addr, bool prediction_correct, bool squashed, const StaticInstPtr & inst, Addr corrTarget);
+
+//prajyotg :: TODO :: not sure if it'll be used
+// Keeping it for now
+  private:
+    /** Returns the index into the LVPT, based on the branch's PC.
+     *  @param inst_PC The branch to look up.
+     *  @return Returns the index into the LVPT.
+     */
+    inline unsigned getIndex(Addr instPC, ThreadID tid);
+
+    /** Returns the tag bits of a given address.
+     *  @param inst_PC The branch's address.
+     *  @return Returns the tag bits.
+     */
+    inline Addr getTag(Addr instPC);
+
+    /** The actual LVPT declaration */
+    std::vector<LVPTEntry> LVPT;
+
+    /** The number of entries in the LVPT. */
     const unsigned numEntries;
 
-    /** Number of bits of the local predictor's counters. */
+    /** Depth of data history kept in the LVPT*/
     const unsigned historyDepth;
 
-    /** Mask to get index bits. */
-    const unsigned indexMask;
+//prajyotg :: No sure if below will be used
+    /** The index mask. */
+    unsigned idxMask;
 
-    /** Number of bits to shift instructions by for predictor addresses. */
-    const unsigned instShiftAmt;
+    /** The number of tag bits per entry. */
+    unsigned tagBits;
+
+    /** The tag mask. */
+    unsigned tagMask;
+
+    /** Number of bits to shift PC when calculating index. */
+    unsigned instShiftAmt;
+
+    /** Number of bits to shift PC when calculating tag. */
+    unsigned tagShiftAmt;
+
+    /** Log2 NumThreads used for hashing threadid */
+    unsigned log2NumThreads;
 };
 
-#endif // __CPU_LVP_LOADVALUEPREDICTIONTABLE_HH__
+#endif // __CPU_PRED_LVPT_HH__
