a13c24e8848cdb9794bf6da697987151e7e49678
Robert Viramontes <rviramontes24@gmail.com>
2021-04-28 14:36:14 -0500

    Updated defaults.

--- gem5/minor_hello_world.sh
+++ gem5/minor_hello_world.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-build/X86/gem5.opt configs/example/se.py --cmd=tests/test-progs/hello/bin/x86/linux/hello --caches --cpu-type=MinorCPU --lct-entries=256  --lct-ctr-bits=2 --lvpt-entries=512 --lvpt-hist-depth=4 --cvu-entries=4 --cvu-replacement=2 | tee run.log
+build/X86/gem5.opt configs/example/se.py --cmd=tests/test-progs/hello/bin/x86/linux/hello --caches --cpu-type=MinorCPU --lct-entries=512  --lct-ctr-bits=2 --lvpt-entries=1024 --lvpt-hist-depth=1 --cvu-entries=8 --cvu-replacement=1 | tee run.log
