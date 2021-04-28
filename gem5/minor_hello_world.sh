0b0eea65fe34cbc6676ed72b60b2164e701b6ec9
Robert Viramontes <rviramontes24@gmail.com>
2021-04-28 12:24:45 -0500

    Added cmd line args.

--- gem5/minor_hello_world.sh
+++ gem5/minor_hello_world.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-build/X86/gem5.opt --debug-flags=LVP,LVPT --debug-start=9999999 configs/example/se.py --cmd=tests/test-progs/hello/bin/x86/linux/hello --caches --cpu-type=MinorCPU 
+build/X86/gem5.opt configs/example/se.py --cmd=tests/test-progs/hello/bin/x86/linux/hello --caches --cpu-type=MinorCPU --lct-entries=256  --lct-ctr-bits=2 --lvpt-entries=512 --lvpt-hist-depth=4 --cvu-entries=4 --cvu-replacement=2 | tee run.log
